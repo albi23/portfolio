f58a59aef9d3e21fccb62e487fc9fbca289fe14a
albi23 <piekielny7891>
2019-11-05 23:18:36 +0100

    Merge remote-tracking branch 'origin/master' into gh-pages 2

---
+++ main.js
@@ -0,0 +1,17 @@
+let idActiveEl = "prof";
+
+function changeActive(isDiv) {
+    if (isDiv === idActiveEl) {
+        return;
+    }
+    let latest = document.getElementById(String(idActiveEl));
+    if (latest && latest.classList) {
+        latest.classList.toggle("active");
+    }
+
+    let elem = document.getElementById(isDiv);
+    if (elem && elem.classList) {
+        elem.classList.toggle("active");
+    }
+    idActiveEl = isDiv;
+}
